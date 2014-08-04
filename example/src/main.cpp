b92e466fecb2772cf63c9657685c7ee9ee7f4dab
Elie Zananiri <prisonerjohn@gmail.com>
2014-08-04 10:22:02 -0400

    Replaced explicit GLUT renderer by default renderer.

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,13 +1,10 @@
 #include "ofMain.h"
-#include "ofAppGlutWindow.h"
 #include "ofApp.h"
 
 //========================================================================
 int main( ){
 
-    ofAppGlutWindow window;
-	window.setGlutDisplayString("rgba double samples>=8 depth");
-	ofSetupOpenGL(&window, 800, 600, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(800, 600, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
