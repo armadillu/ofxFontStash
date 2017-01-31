384c93160278a6b8b3e2a5c8386129581c2b5771
Oriol Ferrer Mesia <oriol@uri.cat>
2017-01-31 16:51:46 -0500

    add ci scripts to clone required addons

---
+++ scripts/ci/osx/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+echo "Executing ci/osx/install.sh"
+
+export OF_ROOT=~/openFrameworks
+export OF_ADDONS=$OF_ROOT/addons
+
+ADDONS="armadillu/ofxTimeMeasurements"
+
+cd $OF_ADDONS
+
+for ADDON in $ADDONS
+do
+  echo "Cloning addon '$ADDON' to " `pwd`
+  git clone --depth=1 --branch=$OF_BRANCH https://github.com/$ADDON.git
+done
+
+cd -
